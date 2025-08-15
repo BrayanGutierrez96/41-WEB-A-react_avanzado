d926578aa383f203ae4374fc996c31b70fb50314
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-08-14 21:25:19 -0500

    Se crea la carpeta del dia 2

---
+++ clase-2-useForm/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Formulario from './components/formulario/Formulario'
+
+function App() {
+  return (
+    <div className='flex h-screen w-full justify-center items-center'><Formulario/></div>
+  )
+}
+
+export default App
