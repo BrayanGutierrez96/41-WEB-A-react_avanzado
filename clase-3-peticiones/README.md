5edd8ea97934c9ce4f534a99f977e1820c8973ac
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-08-19 22:21:21 -0500

    se agrega la funcionalidad del scroll infinito

--- clase-3-peticiones/README.md
+++ clase-3-peticiones/README.md
@@ -1,12 +1,26 @@
-# React + Vite
+## Reto de la clase 3 📑
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+**Desarrollar una aplicación web que muestre los personajes del señor de los anillos, consumiendo la API ```https://thronesapi.com/``` dónde tendrán que leer la documentación para entender cómo funcionan las peticiones.**
 
-Currently, two official plugins are available:
+### Requisitos (opcionales) ✔️
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Crear una aplicación web que muestre los personajes del señor de los anillos, consumiendo la API ```https://thronesapi.com/``` dónde tendrán que leer la documentación para entender cómo funcionan las peticiones.
+- Utilizar la biblioteca ```react-infinite-scroll-component``` para mostrar la lista de personajes.
+- Que sea una aplicación basada en ```Vite``` y ```React```.
+- Utilizar el framework ```TailwindCSS``` para darle un estilo a la aplicación.
 
-## Expanding the ESLint configuration
+### Pasos 👣
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+1. Iniciar el proyecto con ```npm init vite@latest``` y seleccionar ```React``` como framework.
+2. Instalar las dependencias necesarias con ```npm install react react-dom react-infinite-scroll-component```.
+3. Crear un archivo ```App.jsx``` y importar las dependencias necesarias.
+4. Dentro del archivo ```App.jsx``` crear la función principal que será la encargada de hacer la petición a la API y mostrar los resultados.
+5. Dentro de ```App.jsx``` renderizar el contenedor principal que envuelve las cards que muestran cada personaje.
+6. Crear el componente ```InfiniteScroll``` que será responsable de mostrar la lista de personajes.
+7.Configurar el componente ```InfiniteScroll``` para que muestre la lista de personajes.
+
+### Valoraciones adicionales ✅
+
+1. Utilizar la biblioteca ```react-infinite-scroll-component``` para mostrar la lista de personajes.
+2. Utilizar el framework ```TailwindCSS``` para darle un estilo a la aplicación.
+3. Que estructure el proyecto basandose en componentes.
--- clase-3-peticiones/src/App.jsx
+++ clase-3-peticiones/src/App.jsx
@@ -1,51 +1,74 @@
 import { useState, useEffect } from "react";
-import { traerPersonajes } from "./services/traerPersonajes.js";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
   const [personajes, setPersonajes] = useState([]);
+  const [pagina, setPagina] = useState(40);
+  const [hasMore, setHasMore] = useState(true);
 
-  const traerPersonajes = async (url) => {
+  const traerPersonajes = async () => {
     try {
+      const url = `https://rickandmortyapi.com/api/character?page=${pagina}`;
       const respuestaJson = await fetch(url);
       if (!respuestaJson) return null;
       const respuesta = await respuestaJson.json();
       if (!respuesta) return null;
-      setPersonajes(respuesta.results);
+      const hayMasPersonajes = Boolean(respuesta.info?.next);
+
+      setPersonajes((prev) => [...prev, ...respuesta.results]);
+      setPagina((prev) => prev + 1);
+      setHasMore(hayMasPersonajes);
     } catch (error) {
       console.log(error);
+      setHasMore(false);
     }
   };
 
   useEffect(() => {
-    traerPersonajes("https://rickandmortyapi.com/api/character/");
+    traerPersonajes();
   }, []);
 
   return (
-    // Contenedor padre
-    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 p-10">
-      {personajes.map((personaje) => {
-        return (
-          <div className="border border-slate-400 pb-10 rounded-md shadow-xl" key={personaje.id}>
-            <div className="w-full bg-blue-500">
-              <img
-                src={personaje.image}
-                alt=""
-                className="w-full"
-              />
+    // Creamos un componente de contenedor de la biblioteca InfiniteScroll
+    <InfiniteScroll
+      dataLength={personajes.length}
+      next={traerPersonajes}
+      hasMore={hasMore}
+      loader={<h4>Cargando...</h4>}
+      endMessage={
+        <p className="text-center text-red-500 mb-10">
+          <b>-------------- Este es el fin -------------</b>
+        </p>
+      }
+    >
+      {/* Creamos un componente de cajas de personajes */}
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 p-10">
+        {/* Recorremos cada personaje y creamos un componente de la caja de personajes */}
+        {personajes.map((personaje, index) => {
+          return (
+            <div
+              className="border border-slate-400 pb-10 rounded-md shadow-xl"
+              key={index}
+            >
+              <div className="w-full bg-blue-500">
+                <img src={personaje.image} alt="" className="w-full" />
+              </div>
+              <div className="flex flex-col p-10 gap-5 ">
+                <h2>
+                  Nombre:{" "}
+                  <span className="font-extrabold">{personaje.name}</span>
+                </h2>
+                <p>
+                  Especie:
+                  <span className="font-extrabold"> {personaje.species}</span>
+                </p>
+                <p>Esta vivo: {personaje.status === "Alive" ? "😀" : "😔"}</p>
+              </div>
             </div>
-            <div className="flex flex-col p-10 gap-5 ">
-              <h2>
-                Nombre: <span className="font-extrabold">{personaje.name}</span>
-              </h2>
-              <p>
-                Especie:<span className="font-extrabold"> {personaje.species}</span>
-              </p>
-              <p>Esta vivo: {personaje.status === "Alive" ?"😀" : "😔"}</p> 
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </InfiniteScroll>
   );
 }
 
